292db6b75f5bc36ff8002d754646f0b1a174e7c4
itsaln <itsaln23@gmail.com>
2022-06-07 17:53:44 +0500

    'models'

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { AuthController } from './auth.controller';
 
-@Module({})
-export class AuthModule {}
+@Module({
+  controllers: [AuthController]
+})
+export class AuthModule {
+}
