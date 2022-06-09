c36b8f7c053ebd4b6dc2625a54d0769edaf95128
itsaln <itsaln23@gmail.com>
2022-06-09 19:47:23 +0500

    register

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common'
 import { TypegooseModule } from 'nestjs-typegoose'
 import { AuthController } from './auth.controller'
+import { UserModel } from './user.model'
+import { AuthService } from './auth.service';
 
 @Module({
 	controllers: [AuthController],
 	imports: [
 		TypegooseModule.forFeature([
 			{
-				typegooseClass: AuthModule,
-				schemaOptions: { collection: 'Auth' }
+				typegooseClass: UserModel,
+				schemaOptions: { collection: 'User' }
 			}
 		])
-	]
+	],
+	providers: [AuthService]
 })
 
 export class AuthModule {}
